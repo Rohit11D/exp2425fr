a1dcb0bb988f5164946820b85fc368ed6ce9b588
Rohit11D <113600780+Rohit11D@users.noreply.github.com>
2024-10-30 11:24:23 +0530

    1st commit
    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// <!-- import './Home.css'; -->
+// <!-- import { useAuth } from './AuthContext'; -->
+
+const Home = () => {
+    // const { isAuthenticated, userRole, logout } = useAuth();
+    return (
+        <h1>hello</h1>
+    );
+};
+
+export default Home;
